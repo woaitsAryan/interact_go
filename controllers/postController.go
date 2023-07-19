6d6defa00d18a02ac3134e798f58f733995d4cbf
Pratham-Mishra04 <prathamm0409@gmail.com>
2023-07-19 17:16:07 +0530

    feat: minor changes

--- controllers/postController.go
+++ controllers/postController.go
@@ -128,7 +128,7 @@ func AddPost(c *fiber.Ctx) error {
 		return &fiber.Error{Code: 500, Message: "Database Error."}
 	}
 
-	return c.Status(200).JSON(fiber.Map{
+	return c.Status(201).JSON(fiber.Map{
 		"status":  "success",
 		"message": "Post Added",
 		"post":    post,
--- controllers/projectController.go
+++ controllers/projectController.go
@@ -238,7 +238,7 @@ func AddProject(c *fiber.Ctx) error {
 		return &fiber.Error{Code: 500, Message: "Internal Server Error while creating project"}
 	}
 
-	return c.Status(200).JSON(fiber.Map{
+	return c.Status(201).JSON(fiber.Map{
 		"status":  "success",
 		"message": "Project Added",
 		"project": newProject,
