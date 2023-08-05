4ed3d2209fc1a0ccb2ad6efb5a41236b3a1cd9b6
Pratham-Mishra04 <prathamm0409@gmail.com>
2023-08-05 12:01:28 +0530

    feat: search enhancements

--- schemas/post_schemas.go
+++ schemas/post_schemas.go
@@ -1,11 +1,11 @@
 package schemas
 
 type PostCreateSchema struct { // from request
-	Content string   `json:"content" validate:"required,max=500"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags" validate:"dive,alphanum"`
 }
 
 type PostUpdateSchema struct {
-	Content string   `json:"content" validate:"max=500"`
+	Content string   `json:"content" validate:"max=1000"`
 	Tags    []string `json:"tags" validate:"dive,alphanum"`
 }
--- utils/APIFeatures/search.go
+++ utils/APIFeatures/search.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"regexp"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
@@ -15,84 +14,36 @@ func Search(c *fiber.Ctx, index int) func(db *gorm.DB) *gorm.DB {
 			return db
 		}
 
-		regexArry := make([]string, 0)
-		searchComponents := strings.Split(searchStr, " ")
-
-		for _, item := range searchComponents {
-			regexArry = append(regexArry, regexp.QuoteMeta(item))
-		}
-
-		regexArry = append(regexArry, regexp.QuoteMeta(searchStr))
-		regexArry = append(regexArry, regexp.QuoteMeta(strings.ReplaceAll(searchStr, " ", "")))
-
-		interfaceArry := make([]interface{}, len(regexArry))
-		for i, v := range regexArry {
-			interfaceArry[i] = v
-		}
-
 		searchStr = strings.ToLower(searchStr)
 
-		// var searchCondition interface{}
+		substrings := strings.Split(searchStr, " ")
+		var regexPatterns []string
+		for _, substring := range substrings {
+			pattern := ".*" + substring + ".*"
+			regexPatterns = append(regexPatterns, pattern)
+		}
+
 		switch index {
 		case 0: //* users
-			// searchCondition = []interface{}{
-			// 	map[string]interface{}{
-			// 		"username": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"name": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// }
-			db = db.Where("LOWER(name) LIKE ? OR LOWER(username) LIKE ? OR ? = ANY (tags)", "%"+searchStr+"%", "%"+searchStr+"%", searchStr)
+			for _, pattern := range regexPatterns {
+				db = db.Or("LOWER(name) ~ ? OR LOWER(username) ~ ? OR ? = ANY (tags)", pattern, pattern, searchStr)
+			}
 			return db
 		case 1: //* projects
-			// searchCondition = []interface{}{
-			// 	map[string]interface{}{
-			// 		"title": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"tags": gorm.Expr("$elemMatch", map[string]interface{}{
-			// 			"$in": interfaceArry,
-			// 		}),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"category": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// }
-			db = db.Where("LOWER(title) LIKE ? OR ? = ANY (tags)", "%"+searchStr+"%", searchStr)
+			for _, pattern := range regexPatterns {
+				db = db.Or("LOWER(title) ~ ? OR ? = ANY (tags)", pattern, searchStr)
+			}
 			return db
 		case 2: //* posts
-			// searchCondition = []interface{}{
-			// 	map[string]interface{}{
-			// 		"content": gorm.Expr("content IN (?)", interfaceArry),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"tags": gorm.Expr("$elemMatch", map[string]interface{}{
-			// 			"$in": interfaceArry,
-			// 		}),
-			// 	},
-			// }
-			db = db.Where("LOWER(content) LIKE ? OR ? = ANY (tags) ", "%"+searchStr+"%", searchStr)
+			for _, pattern := range regexPatterns {
+				db = db.Or("LOWER(content) ~ ? OR ? = ANY (tags) ", pattern, searchStr)
+			}
 			return db
 		case 3: //* openings
-			// searchCondition = []interface{}{
-			// 	map[string]interface{}{
-			// 		"title": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"description": gorm.Expr("IN (?)", interfaceArry),
-			// 	},
-			// 	map[string]interface{}{
-			// 		"tags": gorm.Expr("$elemMatch", map[string]interface{}{
-			// 			"$in": interfaceArry,
-			// 		}),
-			// 	},
-			// }
 			db = db.Where("LOWER(title) LIKE ? OR ? = ANY (tags)  ", "%"+searchStr+"%", searchStr)
 			return db
 		default:
 			return db
-			// searchCondition = nil
 		}
 	}
 }
